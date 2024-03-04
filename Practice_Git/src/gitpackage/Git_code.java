606885de78e107eab65b91fbc0b384ecac2e37a0
salokhan <salokhan@LIN-7HTYHP3.corp.capgemini.com>
2024-03-04 12:18:12 +0530

    uuuu

---
+++ Practice_Git/src/gitpackage/Git_code.java
@@ -0,0 +1,15 @@
+package gitpackage;
+
+
+
+public class Git_code {
+	
+		public static void main(String args[]) {
+			int a=9;
+			int b=8;
+			System.out.println(a+b);
+		}
+
+	
+
+}
---
+++ Practice_Git/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author salokhan
+ *
+ */
+module Practice_Git {
+}
